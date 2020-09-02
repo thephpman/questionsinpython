3084184aa28627d6449da35edbdde6932b8a8793
Kenshin Himura <thephpman@users.noreply.github.com>
2020-09-02 16:00:47 +0600

    Update function.py

--- function.py
+++ function.py
@@ -18,20 +18,17 @@ persontype1 = "Human"
 persontype2 = "Shel Fish"
 persontype3 = 3333333
 
-contain_digit = True
-
-def custom_Function(name, type):
-    if name.isnumeric() == contain_digit:
-        # Returning the value that its a number,
-        # Name can not be a number
-        return "The name contain number"
-    else:
-        return "Name is passed"
-    if type.isnumeric() == contain_digit:
-        return "The type also contain number please check the inputed data"
-    else:
-        return "Type is passed"
-
-check_names = custom_Function(person1, persontype1)
-
-print(check_names)
+# contain_digit = True
+# #
+# # def custom_Function(name):
+# #     if name.isnumeric() == contain_digit:
+# #         # Returning the value that its a number,
+# #         # Name can not be a number
+# #         return "The name contain number"
+# #     else:
+# #         return "Name is passed"
+# # That function is unable to process.
+#
+# check_names = custom_Function(person3)
+#
+# print(check_names)
